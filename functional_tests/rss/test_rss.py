5a34dab659ec0290eb1b019e8b9891e859995c3c
Dávid Hanko <david.hanko@cesnet.cz>
2026-09-01 12:12:04 +0200

    feat: add per-test pcap selection and RSS testing pcaps
    - TrexTestRun: optional pcap argument restricting TRex traffic to a
      single profile pcap (set_pcap on BaseTrexClientManager)
    - STF profile is rebuilt and re-uploaded when the pcap changes
    - add generated RSS test pcaps (flow A, flows B/C/D, reverse flow rA)
    - move performance test profiles under traffic_profiles/performance_tests/
      and switch tests to absolute imports

---
+++ functional_tests/rss/test_rss.py
@@ -0,0 +1,112 @@
+"""
+Author(s):  Dávid Hanko <david.hanko@cesnet.cz>
+
+Copyright: (C) 2023 - 2026 CESNET, z.s.p.o.
+SPDX-License-Identifier: BSD-3-Clause
+
+Suricata testing module.
+"""
+
+import pytest
+import signal
+import logging
+
+from typing import List
+from lbr_testsuite import trex
+from util.suricata_manager import Suricata_manager
+from util.suri_util import TestInfo
+from assets.trex.traffic_profiles.http_https_smb_trex_profile.profile import (
+    HttpHttpsSmbProfile,
+)
+from conftest import kill_pytest, suri_interface_bind, Suri_conf
+from util.trex_util import TrexMode, get_trex_mode
+from util.multiplier_iterator import multiplier_iterator_create
+from util.test_runner import TrexTestRun
+
+logger = logging.getLogger(__name__)
+
+
+def test_rss(
+    request: pytest.FixtureRequest,
+    trex_generators: dict,
+    result_path: str,
+    suricata_tmp_stats_path: str,
+    utilized_programs_info: dict,
+    params: dict,
+    suri_conf: Suri_conf,
+    get_settings_file: str,
+    get_traffic_duration: int,
+    get_heatup_duration: int,
+    rules_config: dict,
+    get_target_mac: str,
+    get_target_vlan: int,
+):
+    trex_manager: trex.TRexManager = trex.TRexManager(
+        trex.TRexMachinesPool(trex_generators)
+    )
+
+    suri_daemon: Suricata_manager = Suricata_manager(
+        request,
+        suricata_tmp_stats_path,
+        interface=suri_interface_bind(request)[0],
+        capture_mode=suri_interface_bind(request)[1],
+        conf_file=suri_conf.conf_file.with_params(params).build(),
+        rules_file=rules_config["path"],
+    )
+    signal.signal(signal.SIGINT, kill_pytest)
+
+    test_info = TestInfo(
+        result_path=result_path,
+        traffic_duration=get_traffic_duration,
+        heatup_duration=get_heatup_duration,
+        suricata_path_to_bin=suri_daemon.get_path_to_binary(),
+        suricata_rules_paths=[suri_daemon.rules_file],
+        suricata_config_path=suri_daemon.conf_file,
+        utilized_programs_info=utilized_programs_info,
+    )
+
+    trex_mode = get_trex_mode(request, [TrexMode.ASTF, TrexMode.STF])
+    trex_client = HttpHttpsSmbProfile(
+        trex_manager, request, get_target_mac, get_target_vlan, mode=trex_mode
+    )
+
+    test_stages: List[float] = [1, 2, 3]
+
+    stage_iter = multiplier_iterator_create(False, test_stages)
+    for stage in stage_iter:
+        logger.progress(
+            f"Stage {stage} | param_file={request.config.getoption('--param-file')} | params={params}"
+        )
+        match stage:
+            case 1:
+                tester = TrexTestRun(
+                    trex_client,
+                    suri_daemon,
+                    test_info,
+                    params,
+                    request,
+                )
+                tester.execute()
+                pass
+            case 2:
+                tester = TrexTestRun(
+                    trex_client,
+                    suri_daemon,
+                    test_info,
+                    params,
+                    request,
+                )
+                tester.execute()
+                pass
+            case 3:
+                tester = TrexTestRun(
+                    trex_client,
+                    suri_daemon,
+                    test_info,
+                    params,
+                    request,
+                )
+                tester.execute()
+                pass
+
+        logger.info("Run ended.")
